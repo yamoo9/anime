6f69a526477c32796157f98f2d3ae7e5c97c1ed9
Julian Garnier <julian.garnier@gmail.com>
2019-07-25 15:46:49 +0200

    bump version number

--- src/index.js
+++ src/index.js
@@ -1255,7 +1255,7 @@ function timeline(params = {}) {
   return tl;
 }
 
-anime.version = '3.0.1';
+anime.version = '3.1.0';
 anime.speed = 1;
 anime.running = activeInstances;
 anime.remove = removeTargets;
