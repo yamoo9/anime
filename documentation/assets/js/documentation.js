1a0b1608808f10a91329fad6b5959f26695917cd
yamoo9 <yamoo9@naver.com>
2020-01-21 16:58:47 +0900

    영문 한글화 및 개행

--- documentation/assets/js/documentation.js
+++ documentation/assets/js/documentation.js
@@ -6,41 +6,45 @@ var descriptionEl = document.querySelector('.info-output');
 var descriptionTitleEl = document.querySelector('.demo-info h2');
 var demos = [];
 
-function getScrollTop() {
+function getScrollTop(){
   return document.body.scrollTop || document.documentElement.scrollTop;
 }
 
-function scrollTo(selector, offset, cb) {
+function scrollTo(selector, offset, cb){
   var offset = offset || 0;
   var el = document.querySelector(selector);
   var scrollAnim = anime({
-    targets: {scroll: demosEl.scrollTop},
+    targets: { scroll: demosEl.scrollTop },
     scroll: el.offsetTop - offset,
     duration: 500,
     easing: 'easeInOutQuart',
-    update: function(a) { demosEl.scrollTop = a.animations[0].currentValue; },
-    complete: function() { if (cb) cb(); }
+    update: function(a){
+      demosEl.scrollTop = a.animations[0].currentValue;
+    },
+    complete: function(){
+      if (cb) cb();
+    },
   });
 }
 
-function parseJS(demoCode) {
+function parseJS(demoCode){
   var split = demoCode.split('/*DEMO*/\n');
   return split[1] || '';
 }
 
-function createCodePreview(code) {
+function createCodePreview(code){
   var previewEl = document.createElement('div');
   var preEl = document.createElement('pre');
   var codeEl = document.createElement('code');
   previewEl.classList.add('code-preview');
-  previewEl.innerHTML = '<h2>Code example</h2>';
+  previewEl.innerHTML = '<h2>코드 예제(Code example)</h2>';
   codeEl.appendChild(code);
   preEl.appendChild(codeEl);
   previewEl.appendChild(preEl);
   return previewEl;
 }
 
-function outputCode(demoCode, demoTitle, demoDecription, demoColorClass) {
+function outputCode(demoCode, demoTitle, demoDecription, demoColorClass){
   var js = document.createTextNode(parseJS(demoCode));
   demoInfoEl.classList.remove(demoInfoEl.classList[2]);
   demoInfoEl.classList.add(demoColorClass);
@@ -53,7 +57,7 @@ function outputCode(demoCode, demoTitle, demoDecription, demoColorClass) {
   }
 }
 
-function toggleSectionLink(ulEl) {
+function toggleSectionLink(ulEl){
   var ulEls = document.querySelectorAll('.navigation ul');
   var ulLiEls = ulEl.querySelectorAll('.li');
   for (var i = 0; i < ulEls.length; i++) ulEls[i].classList.remove('active');
@@ -63,23 +67,24 @@ function toggleSectionLink(ulEl) {
     targets: '.navigation ul:not(.active)',
     height: 30,
     duration: 400,
-    easing: 'easeOutQuart'
+    easing: 'easeOutQuart',
   });
   anime({
     targets: ulEl,
-    height: function(el) {
+    height: function(el){
       var height = 0;
       var childNodes = el.childNodes;
-      for (var i = 0; i < childNodes.length; i++) height += childNodes[i].offsetHeight;
+      for (var i = 0; i < childNodes.length; i++)
+        height += childNodes[i].offsetHeight;
       return height;
     },
     duration: 600,
     delay: 400,
-    easing: 'easeInOutQuart'
+    easing: 'easeInOutQuart',
   });
 }
 
-function resetDemo() {
+function resetDemo(){
   var els = document.querySelectorAll('.el');
   for (var i = 0; i < els.length; i++) {
     anime.remove(els[i]);
@@ -87,7 +92,7 @@ function resetDemo() {
   }
 }
 
-function resetDemos() {
+function resetDemos(){
   for (var i = 0; i < anime.running.length; i++) {
     var anim = anime.running[i];
     anim.pause();
@@ -96,7 +101,7 @@ function resetDemos() {
   document.body.classList.add('ready');
 }
 
-function createDemo(el) {
+function createDemo(el){
   var demo = {};
   var demoColorClass = el.parentNode.classList[0];
   var scriptEl = el.querySelector('script');
@@ -106,12 +111,14 @@ function createDemo(el) {
   var id = el.id;
   var demoAnim = window[id];
   var demoCode = scriptEl ? scriptEl.innerHTML : '';
-  var demoDescription = descriptionContentEl ? descriptionContentEl.innerHTML : '';
-  function restart() {
+  var demoDescription = descriptionContentEl
+    ? descriptionContentEl.innerHTML
+    : '';
+  function restart(){
     resetDemo();
     demoAnim();
   }
-  function highlightDemo(e, push) {
+  function highlightDemo(e, push){
     var canRestart = !el.classList.contains('controls');
     if (e) {
       e.preventDefault();
@@ -129,30 +136,31 @@ function createDemo(el) {
         //d.anim.pause();
       }
       outputCode(demoCode, demoTitle, demoDescription, demoColorClass);
-      var linkEl = document.querySelector('a[href="#'+id+'"]');
+      var linkEl = document.querySelector('a[href="#' + id + '"]');
       var ulEl = linkEl.parentNode.parentNode;
       linkEl.parentNode.classList.add('active');
       el.classList.add('active');
-      scrollTo('#'+id, 60, function() {
+      scrollTo('#' + id, 60, function(){
         toggleSectionLink(ulEl);
         if (canRestart) restart();
       });
-      if (push) history.pushState(null, null, '#'+id);
-    } else {
+      if (push) history.pushState(null, null, '#' + id);
+    }
+    else {
       if (canRestart) restart();
     }
   }
-  function enterDemo() {
+  function enterDemo(){
     if (!el.classList.contains('active')) {
       restart();
     }
   }
-  function leaveDemo() {
+  function leaveDemo(){
     if (!el.classList.contains('active')) {
       resetDemo();
     }
   }
-  el.addEventListener('click', function(e) {
+  el.addEventListener('click', function(e){
     highlightDemo(e, true);
   });
   resetDemos();
@@ -161,24 +169,26 @@ function createDemo(el) {
     title: demoTitle,
     id: id,
     anim: demoAnim,
-    highlight: highlightDemo
-  }
+    highlight: highlightDemo,
+  };
 }
 
-function getDemoById(id) {
-  return demos.filter(function(a) { return a.id === id})[0];
+function getDemoById(id){
+  return demos.filter(function(a){
+    return a.id === id;
+  })[0];
 }
 
-function createLinksSection(articleEl) {
+function createLinksSection(articleEl){
   var articleId = articleEl.id;
   var articleTitle = articleEl.querySelector('h2').innerHTML;
   var colorClass = articleEl.classList[0];
   var ulEl = document.createElement('ul');
   var liEl = document.createElement('li');
   var sectionLinkEl = document.createElement('a');
-  sectionLinkEl.setAttribute('href', '#'+articleId);
+  sectionLinkEl.setAttribute('href', '#' + articleId);
   sectionLinkEl.innerHTML = articleTitle;
-  sectionLinkEl.addEventListener('click', function(e) {
+  sectionLinkEl.addEventListener('click', function(e){
     e.preventDefault();
     var firstDemoId = articleEl.querySelector('.demo').id;
     var firstDemo = getDemoById(firstDemoId);
@@ -190,13 +200,13 @@ function createLinksSection(articleEl) {
   return ulEl;
 }
 
-function createDemoLink(demo) {
+function createDemoLink(demo){
   var liEl = document.createElement('li');
   var demoLinkEl = document.createElement('a');
-  demoLinkEl.setAttribute('href', '#'+demo.id);
+  demoLinkEl.setAttribute('href', '#' + demo.id);
   demoLinkEl.innerHTML = demo.title;
   demoLinkEl.classList.add('demo-link');
-  demoLinkEl.addEventListener('click', function(e) {
+  demoLinkEl.addEventListener('click', function(e){
     demo.highlight(e, true);
   });
   liEl.appendChild(demoLinkEl);
@@ -220,33 +230,42 @@ for (var i = 0; i < articleEls.length; i++) {
 
 navigationEl.appendChild(fragment);
 
-function updateDemos() {
+function updateDemos(){
   var hash = window.location.hash;
   if (hash) {
-    var id = hash.replace('#','');
+    var id = hash.replace('#', '');
     var demo = getDemoById(id);
     if (demo) demo.highlight();
-  } else {
+  }
+  else {
     demos[0].highlight();
   }
 }
 
-function keyboardNavigation(e) {
+function keyboardNavigation(e){
   var activeDemoEl = document.querySelector('.demo.active');
   switch (e.keyCode) {
     case 38:
       var prevEl = activeDemoEl.previousElementSibling;
-      while (prevEl && !prevEl.classList.contains('demo') && prevEl.parentNode.previousElementSibling) {
+      while (
+        prevEl &&
+        !prevEl.classList.contains('demo') &&
+        prevEl.parentNode.previousElementSibling
+      ) {
         prevEl = prevEl.parentNode.previousElementSibling.lastElementChild;
       }
-      if (prevEl && prevEl.classList.contains('demo')) getDemoById(prevEl.id).highlight(e, true);
+      if (prevEl && prevEl.classList.contains('demo'))
+        getDemoById(prevEl.id).highlight(e, true);
       break;
     case 40:
       var nextEl = activeDemoEl.nextElementSibling;
       if (!nextEl && activeDemoEl.parentNode.nextElementSibling) {
-        nextEl = activeDemoEl.parentNode.nextElementSibling.querySelector('.demo');
+        nextEl = activeDemoEl.parentNode.nextElementSibling.querySelector(
+          '.demo'
+        );
       }
-      if (nextEl && nextEl.classList.contains('demo')) getDemoById(nextEl.id).highlight(e, true);
+      if (nextEl && nextEl.classList.contains('demo'))
+        getDemoById(nextEl.id).highlight(e, true);
       break;
   }
 }
